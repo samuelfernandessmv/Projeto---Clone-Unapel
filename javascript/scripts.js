452ba2f6a42f334bf8fcb7b5bf77384b894304d7
samuelfernandessmv <samuel700500.estudante@gmail.com>
2022-12-02 22:58:14 -0300

    banner animado dnv

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,28 @@
- 
+const slides = document.querySelectorAll (".banner");
+const dots = document.querySelectorAll(".dot");
+let slideIndex = 0;
+
+//Funções
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+      dots[i].classList.remove("active");
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].classList.add("active");
+    dots[slideIndex - 1].classList.add("active");
+
+    setTimeout(showSlides, 4000);
+  }
+
+showSlides ();
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
   spaceBetween: 25,
