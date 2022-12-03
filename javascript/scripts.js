b863928db3245a62724b6de5a3d6f634b8a8a001
samuelfernandessmv <samuel700500.estudante@gmail.com>
2022-12-02 22:40:10 -0300

    criação página conheça nossos equipamentos e rodapé

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,25 +1,30 @@
-const slides = document.querySelectorAll (".banner");
-const dots = document.querySelectorAll(".dot");
-let slideIndex = 0;
+ 
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-//Funções
-
-function showSlides() {
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("active");
-      dots[i].classList.remove("active");
-    }
-  
-    slideIndex++;
-  
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-  
-    slides[slideIndex - 1].classList.add("active");
-    dots[slideIndex - 1].classList.add("active");
-  
-    setTimeout(showSlides, 4000);
-  }
-
-showSlides ();
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
